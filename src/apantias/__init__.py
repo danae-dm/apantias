9255515966e1214b421fcd7d8cd4279da6e34880
Florian Heinrich <florianheinrich@gmx.at>
2026-04-04 23:34:11 +0200

    fresh start with dask

--- src/apantias/__init__.py
+++ src/apantias/__init__.py
@@ -8,11 +8,4 @@ __version__ = importlib.metadata.version("apantias")
 __author__ = "Florian Heinrich"
 __credits__ = "HEPHY Vienna"
 
-from . import analysis
-from . import display
-from . import utils
-from . import file_io
-from . import bin_to_h5
-from .standard import Default
-
 print(f"APANTIAS version {__version__} loaded.")
